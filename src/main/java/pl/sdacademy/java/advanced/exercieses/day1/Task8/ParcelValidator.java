0ee690c4cf1ede345f236b2b603935217992e9c5
Witold Swiatowy <swiatowywitold@gmail.com>
2022-05-22 15:24:46 +0200

    commit Task8 Validator, poprawione że działa

--- src/main/java/pl/sdacademy/java/advanced/exercieses/day1/Task8/ParcelValidator.java
+++ src/main/java/pl/sdacademy/java/advanced/exercieses/day1/Task8/ParcelValidator.java
@@ -2,7 +2,6 @@ package pl.sdacademy.java.advanced.exercieses.day1.Task8;
 
 public class ParcelValidator implements Validator {
 
-
     private final int MAX_VOLUME = 300;
     private final int MIN_LENGHT = 30;
 
@@ -17,15 +16,17 @@ public class ParcelValidator implements Validator {
     }
 
     private boolean validatePerVolume(Parcel input) {
-        return input.getXLenght() + input.getYLenght() + input.getZLenght() < MAX_VOLUME;
+        return input.getXLenght() + input.getYLenght() + input.getZLenght() <= MAX_VOLUME;
     }
 
     private boolean validatePerLenght(Parcel input) {
-        return (input.getXLenght() > MIN_LENGHT && input.getYLenght() > MIN_LENGHT && input.getZLenght() > MIN_LENGHT);
+        return input.getXLenght() >= MIN_LENGHT
+                && input.getYLenght() >= MIN_LENGHT
+                && input.getZLenght() >= MIN_LENGHT;
     }
 
     private boolean validatePerWeight(Parcel input) {
-        if (input.isExpress()) {
+        if (!input.isExpress()) {
             return input.getWeight() < 30.0f;
         } else {
             return input.getWeight() < 15.0f;
--- src/main/java/pl/sdacademy/java/advanced/exercieses/day1/Task8/Validator.java
+++ src/main/java/pl/sdacademy/java/advanced/exercieses/day1/Task8/Validator.java
@@ -3,5 +3,4 @@ package pl.sdacademy.java.advanced.exercieses.day1.Task8;
 public interface Validator {
 
 boolean validate (Parcel input);
-
 }
