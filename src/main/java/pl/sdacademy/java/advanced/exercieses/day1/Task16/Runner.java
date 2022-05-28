30e106fe1789e4d73034cb56b9e6351fbb018aa9
Witold Swiatowy <swiatowywitold@gmail.com>
2022-05-28 13:01:38 +0200

    commit Task 16

---
+++ src/main/java/pl/sdacademy/java/advanced/exercieses/day1/Task16/Runner.java
@@ -0,0 +1,28 @@
+package pl.sdacademy.java.advanced.exercieses.day1.Task16;
+
+import java.util.Arrays;
+
+public enum Runner {
+    BEGINNER(240,360),
+    INTERMEDIATE(180,240),
+    ADVANCE(100,180);
+
+    int minMarathonTime;
+    int maxMarathonTime;
+
+    Runner(int minMarathonTime, int maxMarathonTime) {
+        this.minMarathonTime = minMarathonTime;
+        this.maxMarathonTime = maxMarathonTime;
+    }
+
+    static Runner getFitnessLevel (int time){
+        return Arrays.stream(values())
+                .filter(r -> time > r.minMarathonTime && time <= r.maxMarathonTime)
+                .findFirst()
+//                .orElse(BEGINNER); //jedno z rozwiązań, że zwracamy deufoltowa wartowś
+                .orElseThrow(() -> new IllegalArgumentException("Given time out of possible bound"));
+//        if(time > 240 && time < 360){
+//            return Runner.BEGINNER;
+//        }
+    }
+}
---
+++ src/test/java/pl/sdacademy/java/advanced/exercieses/day1/Task16/RunnerTest.java
@@ -0,0 +1,24 @@
+package pl.sdacademy.java.advanced.exercieses.day1.Task16;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RunnerTest {
+
+    @Test
+    void shouldReturnIntermediateForGivenTimeOf200(){
+        //when
+        Runner result = Runner.getFitnessLevel(200);
+        //then
+        assertThat(result).isEqualTo(Runner.INTERMEDIATE);
+//        Assertions.assertEquals(Runner.INTERMEDIATE, result);
+    }
+    @Test
+    void shouldNotReturnIntermediateForGivenTimeOf300(){
+        //then
+        Assertions.assertThrows(IllegalArgumentException.class, () -> Runner.getFitnessLevel(1000));
+    }
+}
